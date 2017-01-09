f1fd9d3fca5c0e01b553f056b8167e563bf03dd5
alex <alexjo5000@gmail.com>
2017-01-09 10:27:05 -0500

    Updated DataGenerator with JSON.stringify() - still not fully functional

--- test/JoResGameCreator.js
+++ test/JoResGameCreator.js
@@ -8,53 +8,42 @@ function DataGenerator (level) {
     
     // Prints width and height variables
     file += "this.w = " + level.data.length + "; ";
-    file += "this.h = " + level.data[0].length + "; ";
+    file += "this.h = " + level.data[0].block[0].length + "; ";
     
     // Prints tileSize variable
     file += "this.tileSize = " + level.tileSize + "; ";
 
-    file += "this.terrain_sprite =" + level.terrain_sprite + "; ";
+    file += "this.terrain_sprite = '" + level.terrain_sprite + "'; ";
     
     // Prints resources list
-    file += "this.resources = [";
-    for (var i = 0; i < level.resources.length; i++) {
-        file += level.resources[i];
-        if (i !== level.resources.length - 1) file += ",";
+    file += "this.resources = "
+    file += JSON.stringify(level.resources);
+    file += ";";
+    
+     // Prints the unit list
+    file += "this.unit_index = [";
+    for (var i = 0; i < level.unit_index.length; i++) {
+        console.log(JSON.stringify(level.unit_index[i]));
+        file += JSON.stringify(level.unit_index[i]);
+        if (i !== level.sprite_index.length - 1) file += ",";
     }
     file += "]; ";
     
     // Prints the sprite list
     file += "this.sprite_index = [";
     for (var i = 0; i < level.sprite_index.length; i++) {
-        var s = level.sprite_index[i];
-        file += "{sprite:{x:" + s.sprite.x + ",y:" + s.sprite.y; 
-        file += ",w:" + s.sprite.w + ",h:" + s.sprite.h + "},"; 
-        file += "AABB:{x:" + s.AABB.x + ",y:" + s.AABB.y; 
-        file += ",w:" + s.AABB.w + ",h:" + s.AABB.h + "}"; 
-        file += "}";
+        file += JSON.stringify(level.sprite_index[i]);
         if (i !== level.sprite_index.length - 1) file += ",";
     }
     file += "]; ";
     
-    // Prints level.data
+    // Prints level.data slices
     file += "this.data = [\n";
     for (var i = 0; i < level.data.length; i++) {
-        file += "\t[";
-        for (var j = 0; j < level.data[i].length; j++) {
-            file += "\t\t[";
-            for (var k = 0; k < level.data[i][j].length; k++) {
-                file += level.data[i][j][k];
-                if (k !== level.data[i][j].length - 1) file += ",";
-            }
-            file += "]";
-            if (j !== level.data[i].length - 1) file += ",";
-            file += "\n";
-        }
-        file += "]";
+        file += JSON.stringify(level.data[i]);
         if (i !== level.data.length - 1) file += ",";
-        file += "\n";
     }
-    file += "]; ";
+    file += "]";
     
     return file + "}";
 };/**
